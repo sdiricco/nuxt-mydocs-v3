4c3ba376f5044318cd321f9eb915d44827e8a79e
Simone <simo.diricco.91@gmail.com>
2024-07-31 15:43:42 +0200

    add statics pdf

---
+++ components/content/ExternalLink.vue
@@ -0,0 +1,12 @@
+<template>
+  <a :href="src" target="_blank" rel="noopener noreferrer" class="underline-offset-4 underline font-semibold">{{ label
+    }}</a>
+</template>
+
+
+<script setup lang="ts">
+defineProps<{
+  src: string
+  label: string
+}>()
+</script>
---
+++ content/14.computer-science/index.md
@@ -0,0 +1,10 @@
+---
+title: Computer Science
+description: 
+icon: 'iconoir:page'
+---
+
+
+Puoi trovare gli appunti in pdf qua
+::external-link{src="/docs/computer-science/AllNotes.pdf" label="Notes (pdf)"}
+::
